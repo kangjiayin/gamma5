211777699450183706eafdbe01cf57f22186b05e
Your Name <you@example.com>
2021-11-12 13:35:42 +0800

    修改同步文件规则，测试文件

--- src/main.jl
+++ src/main.jl
@@ -1,15 +1,16 @@
 # 总程序
 print("++++++++++++++++++++++++Begin++++++++++++++++++++++++++\n")
 include("./ini.jl")
-plist=[0. ]
+#plist=[i/50 for i=1:50]
+ plist=[0. ]
 lengthplist=length(plist)
 @time for indexforp2=1:lengthplist
     global P2
     P2=plist[indexforp2]
-    include("./integral.jl")
-    include("./eva_kernel_new.jl")
-    include("./kernel_full.jl")
-    jldsave("/Users/kjy/Desktop/program/julia/Gamma5/data/F1k_等间距/F1k$indexforp2.jld2";P2, F1k)
+    include("./mkvar.jl")
+    include("./mkkernel.jl")
+    include("./getsolu.jl")
+    # jldsave("/Users/kjy/Desktop/program/julia/Gamma5/data/F1k_等间距/F1k$indexforp2.jld2";P2, F1k)
     print("$P2 for $indexforp2/$lengthplist done\n")
 end
 
--- src/read.jl
+++ src/read.jl
@@ -7,12 +7,14 @@ k=gausslegendremesh(10. ^-4,10. ^4,32,2);
 P2=Array{Float64}(undef,48,1)
 F1k=Array{Float64}(undef,48,32)
 kname=Array{String}(undef,48,1)
-@time for i=1:48
+
+@time for i=1:32
     local a, b
     global P2,F1k
-    a,b=load("/Users/kjy/Desktop/program/julia/Gamma5/data/F1k_等间距_new1/F1k$i.jld2","P2", "F1k")
+    a,b=load("/Users/kjy/Desktop/program/julia/Gamma5/data/F1k_等间距/F1k$i.jld2","P2", "F1k")
     P2[i]=a
-    F1k[i,:]=1 ./b
+    # F1k[i,:]=1 ./b
+    F1k[i,:]=b
 end #for i
 
 # for i=1:32
